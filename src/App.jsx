30f7b21357ac0b33731dc0450854c3ed35e6b9b1
yukisoba <marquezeuclidgwyne@gmail.com>
2026-04-08 03:17:37 -0700

    coding asgmt week 1
    - set up vite/react template
    - added components to app function (lists)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
 
 function App() {
-  
+  const todoList = [
+    {id: 1, title: "Review Resources"},
+    {id: 2, title: "Take Notes"},
+    {id: 3, title: "Code Out App"}
+  ];
 
   return (
     <div>
       <h1>My Todos</h1>
+      <ul>
+        {todoList.map(todo => <li key={todo.id}>{todo.title}</li>)}
+      </ul>
     </div>
   )
 }
