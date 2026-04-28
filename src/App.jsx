b199aaa82d4ec4a59a39b94bf56a47652e2c8da3
yukisoba <marquezeuclidgwyne@gmail.com>
2026-04-28 00:17:13 -0700

    coding asgmt week 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
+// App.jsx
+import { useState } from 'react';
 import './App.css';
 import TodoList from './TodoList';
 import TodoForm from './TodoForm';
+
+const todos = [
+  {id: 1, title: "Review Resources"},
+  {id: 2, title: "Take Notes"},
+  {id: 3, title: "Code Out App"}
+];
+
 function App() {
+  const [todoList, setTodoList] = useState(todos);
 
   return (
     <div>
       <h1>My Todos</h1>
       <TodoForm/>
-      <TodoList/>
+      <TodoList todoList={todoList}/>
     </div>
   )
 }
