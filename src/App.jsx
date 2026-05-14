0755774fdfdbcc99eaf0c5558cab1190bc66c27a
yukisoba <marquezeuclidgwyne@gmail.com>
2026-05-13 23:24:09 -0700

    Week 5 Coding Asgmt

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,30 @@ function App() {
   function addTodo(todoTitle){
     const newTodo = {
       id: Date.now(),
-      title: todoTitle
+      title: todoTitle,
+      isCompleted: false
     };
 
     setTodoList(previous => [newTodo, ...previous]);
+  }
+
+  function completeTodo(id){
+    const newTodo = todoList.map(todo => {
+      if (todo.id === id){
+        return{...todo, isCompleted:true};
+      }
+
+      return todo;
+    });
+
+    setTodoList(newTodo);
   }
 
   return (
     <div>
       <h1>My Todos</h1>
       <TodoForm onAddTodo={addTodo}/>
-      <TodoList todoList={todoList}/>
+      <TodoList todoList={todoList} onCompleteTodo={completeTodo}/>
     </div>
   )
 }
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,11 +1,15 @@
 // TodoList.jsx - Handles the list for the app
 import TodoListItem from './TodoListItem'
-function TodoList({todoList}) {
+function TodoList({ todoList, onCompleteTodo }) {
+  const todoListFiltered = todoList.filter(todo=>!todo.isCompleted);
+
   return (
     <> 
-        <ul>
-            {todoList.map(todo => <TodoListItem key={todo.id} todo={todo}/> )}
-        </ul>
+      {todoListFiltered.length === 0 ? 
+        (<p>Add todo above to get started!</p>) // if true
+        : (<ul>{todoListFiltered.map(todo => 
+        <TodoListItem key={todo.id} todo={todo} onCompleteTodo={onCompleteTodo}/> )}</ul>) // if false
+      }
     </>
   );
 }
